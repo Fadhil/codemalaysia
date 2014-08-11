3a5f25b41c7db546a9f65e63786450e8541d28d4
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2014-08-12 00:22:33 +0800

    Initial Repo. Updated .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codemalaysia::Application.config.secret_key_base = '113796cb3b814cb15467911b37a63bdde24672b492c99152b36714d5eb4614c5562fac4fe8114e022c4b2cf361fcd74bf6459f868ba8cfb7d06697680079432f'
