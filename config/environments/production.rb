b398f760b3e1dea9010f4dbf14a5330d696013e3
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2014-08-25 13:11:44 +0800

    END -> ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Codemalaysia::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   ActionMailer::Base.smtp_settings = {
-    :user_name => END['SENDGRID_USERNAME'],
+    :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'codemalaysia.com',
     :address => 'smtp.sendgrid.net',
