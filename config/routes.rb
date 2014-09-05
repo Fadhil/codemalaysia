db03c1a4b941d36a0f34111ed65396d1594fd9e5
Kevin Francis <kfrancis@gmail.com>
2014-09-05 13:29:59 +0800

    Added maps + FB like

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Codemalaysia::Application.routes.draw do
 
   get "contact_us/:form_page" => 'contact_us#form_page', as: 'contact_form'
+  get "location"              => 'high_voltage/pages#show', id: "location"
+  get "collaborate"           => 'high_voltage/pages#show', id: "collaborate"
+  get "learn"                 => 'high_voltage/pages#show', id: "learn"
 
   resource :contact_us, only: [ :create]
   root to: 'high_voltage/pages#show', id: 'index'
