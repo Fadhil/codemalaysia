719bd8e83653cd7615bd2ac4fb0d938d8d2b7353
Kevin Francis <kfrancis@gmail.com>
2014-09-18 15:11:15 +0800

    Added /shedule for Hakim :P

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Codemalaysia::Application.routes.draw do
   get "collaborate"           => 'high_voltage/pages#show', id: "collaborate"
   get "learn"                 => 'high_voltage/pages#show', id: "learn"
   get 'schedule'              => 'high_voltage/pages#show', id: 'schedule'
+  get 'shedule'               => 'high_voltage/pages#show', id: 'schedule'
 
   resource :contact_us, only: [ :create]
   root to: 'high_voltage/pages#show', id: 'index'
